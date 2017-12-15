dc8925eae23f2fef7457b68ebd03e1dc4d05b2ec
liyut <liyutong@zhongzairong.cn>
2017-12-15 16:25:00 +0800

    添加页面 图片验证码功能

---
+++ dubbo--consumer/src/main/java/com/pet/consumer/LoginController.java
@@ -0,0 +1,99 @@
+package com.pet.consumer;
+
+import com.pet.api.DemoService;
+import com.pet.api.LoginService;
+import com.pet.api.model.User;
+import com.pet.consumer.utils.GraphicUtils;
+import com.pet.consumer.utils.ResponseJsonUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Created by liyut on 2017-12-14.
+ */
+@Controller
+
+public class LoginController {
+    @Resource
+    DemoService demoService;
+    @Resource
+    LoginService login;
+
+    @RequestMapping("/test.do")
+    @ResponseBody
+    public String login() {
+        String hello = demoService.sayHello("ricky"); // 执行远程方法
+        System.out.println(hello); // 显示调用结果
+
+        User user = demoService.findUserById(15);
+        System.out.println(user); // 显示调用结果
+        String tis = login.login(8);
+        System.out.println(tis + "woshi"); // 显示调用结果
+        return "1";
+    }
+
+    @RequestMapping("/json.do")
+    @ResponseBody
+    public void json(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> data = new HashMap<String, Object>();
+        Map<String, Object> data2 = new HashMap<String, Object>();
+        data2.put("hh","sd");
+        data2.put("hh2","sd3");
+
+        data2.put("hh3","sd2");
+
+        data.put("date", new Date());
+        data.put("email", "accountwcx@qq.com");
+        data.put("age", 30);
+        data.put("name", "csdn");
+        data.put("array", new int[]{1,2,3,4});
+        data.put("data2", data2);
+
+        ResponseJsonUtils.json(response, data);
+    }
+
+    @RequestMapping("/verify.do")
+    @ResponseBody
+    public void verify(HttpServletRequest request, HttpServletResponse response) {
+        // 获得 当前请求 对应的 会话对象
+        HttpSession session = request.getSession();
+
+        // 从请求中获得 URI ( 统一资源标识符 )
+        String uri = request.getRequestURI();
+        System.out.println("hello : " + uri);
+
+        final int width = 180; // 图片宽度
+        final int height = 40; // 图片高度
+        final String imgType = "jpeg"; // 指定图片格式 (不是指MIME类型)
+        final OutputStream output; // 获得可以向客户端返回图片的输出流
+        try {
+            output = response.getOutputStream();
+            String code = GraphicUtils.create(width, height, imgType, output);
+            // 创建验证码图片并返回图片上的字符串
+            System.out.println("验证码内容: " + code);
+
+            // 建立 uri 和 相应的 验证码 的关联 ( 存储到当前会话对象的属性中 )
+            session.setAttribute(uri, code);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // (字节流)
+
+        System.out.println(session.getAttribute(uri));
+
+}
+
+}
---
+++ dubbo--consumer/src/main/webapp/views/user/Login.jsp
@@ -0,0 +1,49 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liyut
+  Date: 2017-12-15
+  Time: 14:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>NeverGoPet 后台管理</title>
+</head>
+<body>
+<script type="text/javascript">
+
+    function myRefersh( e ) {
+
+        const source = e.src ; // 获得原来的 src 中的内容
+        //console.log( "source : " + source  ) ;
+
+        var index = source.indexOf( "?" ) ;  // 从 source 中寻找 ? 第一次出现的位置 (如果不存在则返回 -1 )
+        //console.log( "index : " + index  ) ;
+
+        if( index > -1 ) { // 如果找到了 ?  就进入内部
+            var s = source.substring( 0 , index ) ; // 从 source 中截取 index 之前的内容 ( index 以及 index 之后的内容都被舍弃 )
+            //console.log( "s : " + s  ) ;
+
+            var date = new Date(); // 创建一个 Date 对象的 一个 实例
+            var time = date.getTime() ; // 从 新创建的 Date 对象的实例中获得该时间对应毫秒值
+            e.src = s + "?time=" + time ; // 将 加了 尾巴 的 地址 重新放入到 src 上
+
+            //console.log( e.src ) ;
+        } else {
+            var date = new Date();
+            e.src = source + "?time=" + date.getTime();
+        }
+
+    }
+</script>
+
+<div>
+    <input type="text" name="userName" id="userName">
+    <input type="text" name="password" id="password">
+    <input type="text" name="verfyCode" id="verfyCode">
+    <img src="/verify.do" onclick="myRefersh(this)">
+</div>
+
+</body>
+</html>
